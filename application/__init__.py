b1b0b7be3dacf0bf9519856fbc28b9b58c9d7ee4
Ravi M Damor <damorravi540@gmail.com>
2024-07-20 18:50:22 +0530

    Add files via upload

---
+++ application/__init__.py
@@ -0,0 +1,15 @@
+"""
+  @author Victor I. Afolabi
+
+  A.I. Engineer & Software developer
+  javafolabi@gmail.com
+
+  Created on 01 February, 2018 @ 2:38 PM.
+  Copyright (c) 2018. Victor. All rights reserved.
+"""
+from .app import app
+
+from .controllers import *
+from .apis import *
+
+__all__ = ["app"]
